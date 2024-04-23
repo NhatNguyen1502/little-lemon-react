0cf8d1b1611a20c5f2a152fe0001a3d29163367e
nguyenlamnhat <zznhatphilong@gmail.com>
2024-04-23 14:22:14 +0700

    set up some sematic tag

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
-import BookingForm from './components/BookingForm';
+import "./App.css";
+import BookingForm from "./components/BookingForm";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <BookingForm />
-    </div>
+    <>
+      <Header />
+      <main>
+        <BookingForm />
+      </main>
+      <Footer />
+    </>
   );
 }
 
