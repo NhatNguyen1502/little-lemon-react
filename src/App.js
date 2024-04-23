d449cacdf6a1028d9babb2d7b0d200062d763a55
nguyenlamnhat <zznhatphilong@gmail.com>
2024-04-23 14:36:44 +0700

    done set up header

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import BookingForm from "./components/BookingForm";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <>
+      <Nav />
       <Header />
       <main>
         <BookingForm />
